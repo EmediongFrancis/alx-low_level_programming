3c01f351f470e856e8aa307ce7ae407daa48391a
Emediong Francis <emediongfrancis@gmail.com>
2021-06-22 09:45:33 -0700

    added holberton file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry block
+ * Description: prints holberton on new line
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+c = "Holberton";
+
+putchar(c);
+return (0);
+}
