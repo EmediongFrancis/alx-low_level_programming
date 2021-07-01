0ec7453ec8c6816065e35262dcb8eadf2dd5a746
Emediong Francis <emediongfrancis@gmail.com>
2021-07-01 12:59:31 -0700

    .

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,103 +1,45 @@
 #include "holberton.h"
 
 /**
- *powB - raises the number base to power power
- *@base : the base
- *@power : the power
- *Return: return the answer
- */
-int powB(unsigned int base, int power)
-{
-	int i, prod = 1;
-
-	for (i = 0; i < power; i++)
-	{
-		prod = prod * base;
-	}
-	return (prod);
-}
-
-/**
- *numLength - returns the lenth of string
- *@num : operand number
- *Return: number of digits
- */
-int numLength(unsigned int num)
-{
-	int length = 0;
-
-	if (!num)
-		return (1);
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-
-	return (length);
-}
-
-/**
- *printnchar - Print a number of any digit
- *@n : takes an input number
+ * print_number - prints an integer
  *
+ * @num: int to print
  *
+ * Return: void
  */
-void printnchar(int n)
-{
-	int length, j, k, digit1, tmp2;
-	unsigned int num;
 
-	if (n < 0)
+void print_number(int num)
+{
+	unsigned int d = 10, n;
+
+	if (num < 0)
 	{
-		n *= -1;
 		_putchar('-');
+		num *= -1;
 	}
-	num = n;
-	length = numLength(num);
-	j = length - 1;
-	if (num == 0)
-		_putchar(48);
+	n = num;
+	if (n < d)
+	{
+		_putchar('0' + n);
+	}
 	else
 	{
-		while (j >= 0)
+		while (n >= d)
 		{
-			if (num % powB(10, j) == 0 && j != 0)
-			{
-				_putchar(48 + num / powB(10, j));
-				for (k = j; k > 0; k--)
-					_putchar(48);
-				j = -1;
-			}
-			else
-			{
-				digit1 = num / powB(10, j);
-				_putchar(digit1 + 48);
-
-				tmp2 = num;
-				num -= powB(10, j) * digit1;
-				if (numLength(tmp2) - numLength(num) == 2)
-				{
-					_putchar(48);
-					j--;
-				}
-				j--;
-			}
+			d *= 10;
+			if (d >= 1000000000)
+			if (d == 1000000000)
+				break;
 		}
+		if (!(d >= 1000000000) || n > 100000000)
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
 	}
-}
-
-
-
-
-/**
- * print_number - prints out any integer with only _putchar
- * @n: integer to be printed
- * Return: 0
- */
-void print_number(int n)
-{
-
-	printnchar(n);
 }
