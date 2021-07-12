bec4d4659554703081636e782d7e6895706189b8
Emediong Francis <emediongfrancis@gmail.com>
2021-07-12 09:10:56 -0700

    arguments 😀

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+/**
+ * main - prints number of arguments, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
